efc2ab6d2e2360a3dcdde9253c0cdbefacd874ad
Emmanuel Oladiran <oladirane@gmail.com>
2023-10-04 08:26:51 +0100

    loading...

---
+++ script.js
@@ -0,0 +1,19 @@
+// const plusin = document.querySelector(".plus");
+// const dropdown = document.querySelector(".faq");
+
+// function removeplus() {
+//   plusin.classList.toggle("active");
+//   dropdown.classList.toggle("active");
+// }
+
+// plusin.addEventListener("click", removeplus);
+
+const plusin = document.querySelectorAll(".plus");
+const dropdown = document.querySelectorAll(".faq");
+
+function removeplus() {
+  plusin.classList.toggle("active");
+  dropdown.classList.toggle("active");
+}
+
+plusin.forEach((item) => item.addEventListener("click", removeplus));
