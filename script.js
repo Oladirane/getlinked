a34ec36b7caa037d1d88252440c9789dc89729a4
Emmanuel Oladiran <oladirane@gmail.com>
2024-03-13 21:38:33 +0100

    updates

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const dropdown = document.querySelectorAll(".faqboss");
 
 dropdown.forEach((item) => {
   const ola = item.querySelector(".plus");
+  const faq = item.querySelector(".faq1");
   const olad = item.querySelector(".faq");
 
-  ola.addEventListener("click", () => {
+  faq.addEventListener("click", () => {
     ola.classList.toggle("active");
     olad.classList.toggle("active");
   });
