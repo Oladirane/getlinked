91bf7ed577b5ac1778ef1e0b46b847c228e2dd8d
Emmanuel Oladiran <oladirane@gmail.com>
2024-03-15 19:25:19 +0100

    mobile view

--- script.js
+++ script.js
@@ -10,32 +10,3 @@ dropdown.forEach((item) => {
     olad.classList.toggle("active");
   });
 });
-
-// function removeplus() {
-//   plusin.classList.toggle("active");
-//   dropdown.classList.toggle("active");
-// }
-
-// plusin.addEventListener("click", removeplus);
-
-// const plusin = document.querySelectorAll(".plus");
-// const dropdown = document.querySelectorAll(".faq");
-
-// function removeplus() {
-//   plusin.classList.toggle("active");
-//   dropdown.classList.toggle("active");
-// }
-
-// plusin.forEach((item) => item.addEventListener("click", removeplus));
-
-// const faqitems = document.querySelectorAll(".faqboss");
-
-// faqitems.forEach((item) => {
-//   const togglebutton = item.querySelector(".plus");
-//   const answer = item.querySelector(".faq");
-
-//   togglebutton.addEventListener("click", () => {
-//     togglebutton.classList.toggle("active");
-//     answer.classList.toggle("active");
-//   });
-// });
